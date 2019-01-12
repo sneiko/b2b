d3583f6c2c868cf734be807bd8df1db1246dba7b
-_- <036006@gmail.com>
2019-01-13 00:09:20 +0300

    go

--- B2BApi/Migrations/20190112205446_MigrationRebase.Designer.cs
+++ B2BApi/Migrations/20190112205446_MigrationRebase.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using B2BApi.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace B2BApi.Migrations
 {
     [DbContext(typeof(B2BDbContext))]
-    partial class B2BDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190112205446_MigrationRebase")]
+    partial class MigrationRebase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
