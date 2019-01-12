e0bc8b14f607f17cd2841c47075361c7d9502d94
-_- <036006@gmail.com>
2019-01-12 14:45:31 +0300

    asdf

--- B2BApi/Controllers/TestController.cs
+++ B2BApi/Controllers/TestController.cs
@@ -6,6 +6,7 @@ using B2BApi.DbContext;
 using B2BApi.Models;
 using B2BApi.Models.Enum;
 using B2BApi.Models.Helpers;
+using B2BApi.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,19 +25,19 @@ namespace B2BApi.Controllers
             using (var context = new B2BDbContext())
             {
                 var product = context.Products
-                        .Include(price => price.Price)
-                        .Single(x => x.Id == 1);
-                
+                    .Include(price => price.Price)
+                    .Single(x => x.Id == 1);
+
                 return new JsonResult(product);
             }
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id) 
+        public ActionResult<string> Get(int id)
         {
             var e = new Excel();
-            var r = e.Parse(id);
+            var r = e.Parse(id, "https://www.dropbox.com/s/kv5bx2ncfz8bzhn/%D0%9D%D0%B0%D0%B4%D0%B5%D0%B6%D0%BD%D1%8B%D0%B5%20%D0%B8%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B.xls?dl=1");
             return new JsonResult(r);
             return "value2";
         }
